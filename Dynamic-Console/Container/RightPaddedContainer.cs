4bc767e30de408e78dcbfa77858d9b88800fea82
Ryan Jennings <ryfoje2@gmail.com>
2024-05-24 22:57:00 -0400

    Created RightPaddedContainer

---
+++ Dynamic-Console/Container/RightPaddedContainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Console
+{
+    public class RightPaddedContainer : SingleContainer
+    {
+
+        public RightPaddedContainer(Content content) : base(content) { }
+
+        public override void Display()
+        {
+            int calculatedPadding = _width - _content.GetWidth();
+            Helper.PrintChar(calculatedPadding, ' ');
+
+            _content.Display();
+        }
+
+    }
+}
