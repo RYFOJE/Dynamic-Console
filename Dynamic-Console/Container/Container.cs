766d40290fef76904911aab004ac6a3e5fb55232
Ryan Jennings <ryfoje2@gmail.com>
2024-05-24 22:06:18 -0400

    Added basic Container that acts as blank line.

--- Dynamic-Console/Container/Container.cs
+++ Dynamic-Console/Container/Container.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,28 @@ namespace Dynamic_Console
     public class Container
     {
 
+        protected int _width;
+        
+        public Container()
+        {
+            _width = Menu.GetWidth();
+        }
+
+        public void SetWidth(int width)
+        {
+            _width = width;
+        }
+
+        public int GetWidth()
+        {
+            return _width;
+        }
+
+
+        public void Display() 
+        {
+            Helper.PrintChar(_width, ' ');
+        }
 
     }
 }
