7bda3851f5c5a9efdcf75746e20284531e887d55
Ryan Jennings <ryfoje2@gmail.com>
2024-05-24 22:22:31 -0400

    Added Content Class

---
+++ Dynamic-Console/Content/Content.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Console.Content
+{
+    public class Content
+    {
+
+        public Content() { }
+
+    }
+}
