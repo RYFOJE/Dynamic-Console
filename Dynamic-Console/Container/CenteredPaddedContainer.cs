31727d8f890deac84d9bb1792068cacd3920ad23
Ryan Jennings <ryfoje2@gmail.com>
2024-05-24 23:48:56 -0400

    Added CenterPaddedContainer

---
+++ Dynamic-Console/Container/CenteredPaddedContainer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Console
+{
+    public class CenteredPaddedContainer : SingleContainer
+    {
+
+        public CenteredPaddedContainer(Content content) : base(content) { }
+
+        public override void Display()
+        {
+            int totalPadding = _width - _content.GetWidth();
+            int leftPadding = totalPadding / 2;
+            int rightPadding = totalPadding - leftPadding;
+
+            Helper.PrintChar(leftPadding, ' ');
+
+            _content.Display();
+
+            Helper.PrintChar(rightPadding, ' ');
+        }
+
+    }
+}
