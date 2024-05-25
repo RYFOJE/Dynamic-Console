36d318e6ccdd1fee87caa65e64a84d8618b04999
Ryan Jennings <ryfoje2@gmail.com>
2024-05-24 22:31:06 -0400

    Created Single Container File

---
+++ Dynamic-Console/Container/SingleContainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Console
+{
+    abstract class SingleContainer(int width) : Container(width)
+    {
+
+        Content _content = new Content();
+
+        public void SetContent(Content newContent)
+        {
+            _content = newContent;
+        }
+
+    }
+}
