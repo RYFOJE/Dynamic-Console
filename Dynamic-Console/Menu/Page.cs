359c7eff12d8ec3b2836d225ef7c20208883d21c
Ryan Jennings <ryfoje2@gmail.com>
2024-05-24 21:13:18 -0400

    Added sections to pages

---
+++ Dynamic-Console/Menu/Page.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Console.Menu
+{
+    public class Page
+    {
+
+        public Section[] _sections;
+
+        public Page()
+        {
+            _sections = new Section[1];
+            _sections[0] = new Section();
+        }
+
+        public Page(int sectionCount)
+        {
+            if (sectionCount < 1) { throw new ArgumentOutOfRangeException("A page must have 1 section or more"); }
+
+            _sections = new Section[sectionCount];
+
+            for(int i = 0; i < sectionCount; i++)
+            {
+                _sections[i] = new Section();
+            }
+        }
+
+        public void Display()
+        {
+            foreach (var item in _sections)
+            {
+                item.Display();
+            }
+        }
+
+    }
+}
