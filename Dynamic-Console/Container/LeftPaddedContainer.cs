4fbe1797fb013b4cb14a3aa9d1f8d567f0c5c21c
Ryan Jennings <ryfoje2@gmail.com>
2024-05-24 22:56:50 -0400

    Created LeftPaddedContainer

---
+++ Dynamic-Console/Container/LeftPaddedContainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Console
+{
+    public class LeftPaddedContainer : SingleContainer
+    {
+
+        public LeftPaddedContainer(Content content) : base(content) { }
+
+        public override void Display()
+        {
+            _content.Display();
+
+            int calculatedPadding = _width - _content.GetWidth();
+            Helper.PrintChar(calculatedPadding, ' ');
+        }
+
+    }
+}
