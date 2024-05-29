10fbd5fc07587975b0d5ba2af80fb3ef037f6ba1
Ryan Jennings <ryfoje2@gmail.com>
2024-05-28 23:19:12 -0400

    Added builder implementation for pages

---
+++ Dynamic-Console/Builder/PageBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Console.Builder
+{
+    internal class PageBuilder
+    {
+
+        private Page _page;
+
+        public PageBuilder CreatePage()
+        {
+            _page = new Page();
+            return this;
+        }
+
+        public PageBuilder CreatePage(int sections)
+        {
+            _page = new Page(sections);
+            return this;
+        }
+
+        public PageBuilder AddDivider(int sectionNumber)
+        {
+            _page.GetSection(sectionNumber).EnableDivider();
+            return this;
+        }
+
+        public PageBuilder AddLine(int sectionIndex, Line line)
+        {
+            _page.GetSection(sectionIndex).AddLine(line);
+            return this;
+        }
+
+        public Page Build()
+        {
+            return _page;
+        }
+    }
+}
